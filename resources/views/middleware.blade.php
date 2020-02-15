b7f250d1b876f28def688d721ae29c594a4107b2
DevYnov1 <alexandre.bonnat@ynov.com>
2020-02-15 17:08:58 +0100

    minor fix V2 page admin

--- resources/views/middleware.blade.php
+++ resources/views/middleware.blade.php
@@ -1,3 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
 <div class=”title m-b-md”>
     {{ strtoupper($message)}} only page!
 </div>
+@endsection
--- resources/views/unauthorized.blade.php
+++ resources/views/unauthorized.blade.php
@@ -1,3 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
 <div class=”title m-b-md”>
     You cannot access this page! This is for only '{{$role}}'"
 </div>
+@endsection
